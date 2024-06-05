23c8d21ebafd16a9528c9a570f93e64c0fe7fbfe
MarcoColloca <marco.colloca3511@gmail.com>
2024-06-05 16:47:49 +0200

    Add → added types CRUD create

--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -23,7 +23,7 @@ class TypeController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.types.create');
     }
 
     /**
---
+++ resources/views/admin/types/create.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('title', 'Character')
+
+@section('content')
+<section class="mt-5 pt-5">
+    <div class="container bg-dark py-4">
+        <h1 class="title text-center text-success">Add a New Type to Database!</h1>
+    </div>
+</section>
+
+
+<section class="mb-5 py-5">
+    <div class="bg-dark text-light container py-4">
+        <form action="{{ route('admin.types.store') }}" method="POST">
+            @csrf
+
+            <div class="mb-3">
+                <label for="name" class="form-label fw-bold">Name</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Insert your new type"
+                    value="{{ old('name') }}">
+            </div>
+
+            <button class="btn btn-primary">Add</button>
+        </form>
+    </div>
+
+    <div class="container mt-4">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{$error}}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+    </div>
+</section>
+@endsection
