295fe8bccdcfd5e0bd5ded93851f25c559fa97b7
MarcoColloca <marco.colloca3511@gmail.com>
2024-06-05 18:02:15 +0200

    Add → added Types CRUD Show

---
+++ resources/views/admin/types/show.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+
+<section class="mb-5 py-5 bg-purple">
+    <div class="bg-light container py-4 types-cotnainer">
+
+        <h1 class="my-4 text-center"> {{$type->name}} </h1>
+        <h4 class="text-center">{{$type->slug}}</h4>
+
+        <div class="row pb-5 w-100 justify-content-center">
+            <div class="card p-5 my-card">
+                <p>
+                    <span class="fw-bold">Description:</span>
+                </p>
+                <p>
+                    Work in Progress..
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+@endsection
