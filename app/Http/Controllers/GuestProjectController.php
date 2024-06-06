f4bfcd4de6ca1c9d372669fdc8e48d628bb9af11
MarcoColloca <marco.colloca3511@gmail.com>
2024-06-06 12:41:59 +0200

    Add → added GuestProject Controller

---
+++ app/Http/Controllers/GuestProjectController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\GuestProject;
+use App\Http\Requests\StoreGuestProjectRequest;
+use App\Http\Requests\UpdateGuestProjectRequest;
+
+class GuestProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreGuestProjectRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(GuestProject $guestProject)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(GuestProject $guestProject)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateGuestProjectRequest $request, GuestProject $guestProject)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(GuestProject $guestProject)
+    {
+        //
+    }
+}
