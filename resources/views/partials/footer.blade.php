13d025137221c1bb41aef2c219f6ca2d4158b4e7
MarcoColloca <marco.colloca3511@gmail.com>
2024-06-06 12:08:46 +0200

    Refactor → changed header appereance

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
         <div class="container">
-            <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
+            <a class="navbar-brand d-flex align-items-center" href="{{ url('https://laravel.com/docs/10.x') }}" target="_blank">
                 <div class="logo_laravel">
                     <svg viewBox="0 0 651 192" fill="none" xmlns="http://www.w3.org/2000/svg" style="width: 150px">
                         <g clip-path="url(#clip0)" fill="#EF3B2D">
