6449046ef8b7a2ce8b674a3c2abc0ecf676b67b7
MarcoColloca <marco.colloca3511@gmail.com>
2024-06-06 16:05:02 +0200

    Refactor  → changed size ot types table in tyeps Index

--- resources/views/admin/types/index.blade.php
+++ resources/views/admin/types/index.blade.php
@@ -12,44 +12,45 @@
             <div class="text-end me-3">
                 <a class="btn btn-dark mb-5" href="{{route('admin.types.create')}}">Add Type</a>
             </div>
-
-            <table class="table table-sm table-dark table-hover table-bordered">
-                <thead>
-                    <tr>
-                        <th scope="col-1">Name</th>
-                        <th scope="col-1">Slug</th>
-                        <th class="text-center" scope="col-1">More Info</th>
-                        <th class="text-center" scope="col-1">Modify</th>
-                        <th class="text-center" scope="col-1">Delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($types as $type)                
-                        <tr class="position-relative">
-                            <td>{{$type->name}}</td>
-                            <td>{{$type->slug}}</td>
-                            <td class="text-center"><a class="text-success" href="{{route('admin.types.show', $type)}}">Info</a></td>
-                            <td class="text-center"><a class="text-primary" href="{{route('admin.types.edit', $type)}}">Edit</a></td>
-                            <td class="text-center">
-                                <form class="item-delete-form" action="{{ route('admin.types.destroy', $type) }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-    
-                                    <button class="btn link-danger">X</button>
-    
-                                    <div class="my-modal">
-                                        <div class="my-modal__box">
-                                            <h5 class="text-center me-5">Do you really want to delete this type?!</h5>
-                                            <span class="link link-danger my-modal-yes mx-5">Yes</span>
-                                            <span class="link link-success my-modal-no">No</span>
-                                        </div>
-                                    </div>
-                                </form>
-                            </td>
+            <div class="table-container">
+                <table class="table table-sm table-dark table-hover table-bordered">
+                    <thead>
+                        <tr>
+                            <th class="text-center" scope="col-1">Name</th>
+                            <th class="text-center" scope="col-1">Slug</th>
+                            <th class="text-center" scope="col-1">More Info</th>
+                            <th class="text-center" scope="col-1">Modify</th>
+                            <th class="text-center" scope="col-1">Delete</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        @foreach ($types as $type)                
+                            <tr class="position-relative">
+                                <td class="text-center">{{$type->name}}</td>
+                                <td class="text-center">{{$type->slug}}</td>
+                                <td class="text-center"><a class="text-success" href="{{route('admin.types.show', $type)}}">Info</a></td>
+                                <td class="text-center"><a class="text-primary" href="{{route('admin.types.edit', $type)}}">Edit</a></td>
+                                <td class="text-center">
+                                    <form class="item-delete-form" action="{{ route('admin.types.destroy', $type) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+        
+                                        <button class="btn link-danger">X</button>
+        
+                                        <div class="my-modal">
+                                            <div class="my-modal__box">
+                                                <h5 class="text-center me-5">Do you really want to delete this type?!</h5>
+                                                <span class="link link-danger my-modal-yes mx-5">Yes</span>
+                                                <span class="link link-success my-modal-no">No</span>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </section>
     
